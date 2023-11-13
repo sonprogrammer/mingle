7ef5b1b4ccf5e1320d2fe1a6a3314f93f4c07624
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-13 17:47:03 +0900

    feature:connect DB and set library dotenv

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+// MongoDB 연결: MongoDB와 연결함
+const conn = require('./db/connect.js');
+conn.MongoConnect();
+
+// dotenv 설정: 환경변수 로드를 위해 dotenv 설정을 적용
+require('dotenv').config();
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
