ee5b5b380b0b14e2f45203a7a9b254a3581bad95
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-14 20:46:19 +0900

    feature : login-register add

--- server/db/schemas/userSchema.js
+++ server/db/schemas/userSchema.js
@@ -1,28 +1,50 @@
-import { Schema } from "mongoose";
+const mongoose = require("mongoose");
 
-export const UserSchema = new Schema({
-  userEmail: { type: String, required: true, unique: true },
-  userId: { type: String, required: true, unique: true },
-  userPassword: { type: String, required: true },
-  userName: { type: String, required: true },
-  userNickname: { type: String, required: true },
-  userImg: { type: String, required: true },
-  userDescription: { type: String, default: "" },
-  likeSong: {
-    type: [{ type: Schema.Types.ObjectId, ref: "Songs" }],
-    default: [],
+const userSchema = new mongoose.Schema(
+  {
+    userEmail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userPassword: {
+      type: String,
+      required: true,
+    },
+    userNickname: {
+      type: String,
+      required: true,
+    },
+    userDescription: {
+      type: String,
+      default: "",
+    },
+    likeSong: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Songs" }],
+      default: [],
+    },
+    likePlayList: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "PlayLists" }],
+      default: [],
+    },
+    userSong: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Songs" }],
+      default: [],
+    },
+    userPlayList: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "PlayLists" }],
+      default: [],
+    },
+    userPreference: {
+      type: [String],
+      default: [],
+    },
   },
-  likePlayList: {
-    type: [{ type: Schema.Types.ObjectId, ref: "PlayLists" }],
-    default: [],
-  },
-  userSong: {
-    type: [{ type: Schema.Types.ObjectId, ref: "Songs" }],
-    default: [],
-  },
-  userPlayList: {
-    type: [{ type: Schema.Types.ObjectId, ref: "PlayLists" }],
-    default: [],
-  },
-  userPreference: { type: [String], default: [] },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+    collection: "user", // 몽고디비 컬렉션 이름 설정
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
