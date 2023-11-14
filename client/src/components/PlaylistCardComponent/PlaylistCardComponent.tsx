94d0aff20117a3bfcf1155454f9ed2a2b6e9a245
Namhyun <jenple9709@gmail.com>
2023-11-14 21:30:37 +0900

    design: Playlist-card-component-ui

--- client/src/components/PlaylistCardComponent/PlaylistCardComponent.tsx
+++ client/src/components/PlaylistCardComponent/PlaylistCardComponent.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { PlaylistCardProps } from "../../types/PlaylistCardProps";
-import { HeartOutlined, CommentOutlined } from "@ant-design/icons";
-
+import {
+  PlaylistCardContainer,
+  ProfileSection,
+  ProfileInfo,
+  ProfileImage,
+  ProfileName,
+  AlbumImage,
+  ContentSection,
+  Title,
+  HashtagList,
+  Hashtag,
+  SocialInfo,
+  LikesText,
+  StyledHeartOutlined,
+  StyledCommentOutlined,
+} from "./styles";
 export const PlaylistCardComponent: React.FC<PlaylistCardProps> = ({
   profileIcon,
   profileName,
@@ -11,37 +25,27 @@ export const PlaylistCardComponent: React.FC<PlaylistCardProps> = ({
   likes,
 }) => {
   return (
-    <div className="flex h-max bg-neutral-400 rounded-lg">
-      <div className="flex flex-col items-start p-4">
-        <div className="flex items-center mb-4">
-          <img
-            src={profileIcon}
-            alt="Profile"
-            className="w-6 h-6 rounded-full mr-2.5 object-cover"
-          />
-          <p className="text-sm font-black text-slate-950">{profileName}</p>
-        </div>
-        <img
-          src={albumCover}
-          alt="Album Cover"
-          className="w-36 h-36 rounded-lg self-start"
-        />
-      </div>
-      <div className="flex-grow p-4 -ml-4">
-        <h3 className="text-lg mt-10 text-black font-semibold">{title}</h3>
-        <ul className="list-none mt-4 mb-4">
+    <PlaylistCardContainer>
+      <ProfileSection>
+        <ProfileInfo>
+          <ProfileImage src={profileIcon} alt="Profile" />
+          <ProfileName>{profileName}</ProfileName>
+        </ProfileInfo>
+        <AlbumImage src={albumCover} alt="Album Cover" />
+      </ProfileSection>
+      <ContentSection>
+        <Title>{title}</Title>
+        <HashtagList>
           {hashtags.map((tag, index) => (
-            <li key={index} className="inline mr-2 text-gray-600 font-bold">
-              #{tag}
-            </li>
+            <Hashtag key={index}>#{tag}</Hashtag>
           ))}
-        </ul>
-        <HeartOutlined className="text-purple-500 mr-4 mb-1 text-3xl" />
-        <CommentOutlined className="text-purple-500 text-3xl" />
-        <div className="social-info">
-          <span className="font-bold text-black">좋아요: {likes}</span>
-        </div>
-      </div>
-    </div>
+        </HashtagList>
+        <StyledHeartOutlined />
+        <StyledCommentOutlined />
+        <SocialInfo>
+          <LikesText>좋아요: {likes}</LikesText>
+        </SocialInfo>
+      </ContentSection>
+    </PlaylistCardContainer>
   );
 };
---
+++ client/src/components/PlaylistCardComponent/styles.ts
@@ -0,0 +1,22 @@
+import tw, { styled } from "twin.macro";
+import { HeartOutlined, CommentOutlined } from "@ant-design/icons";
+
+export const PlaylistCardContainer = tw.div`flex h-max bg-neutral-400 rounded-lg w-1/2`;
+export const ProfileSection = tw.div`flex flex-col items-start p-4`;
+export const ProfileInfo = tw.div`flex items-center mb-4`;
+export const ProfileImage = tw.img`w-8 h-8 rounded-full mr-2.5 object-cover`;
+export const ProfileName = tw.p`text-sm font-black text-slate-900`;
+export const AlbumImage = tw.img`w-36 h-36 rounded-lg self-start`;
+export const ContentSection = tw.div`flex-grow p-4 -ml-4`;
+export const Title = tw.h3`text-lg mt-10 text-black font-semibold`;
+export const HashtagList = tw.ul`list-none mt-4 mb-4`;
+export const Hashtag = tw.li`inline mr-2 text-gray-600 font-bold`;
+export const SocialInfo = tw.div`mt-2`;
+export const LikesText = tw.span`font-bold text-black`;
+
+export const StyledHeartOutlined = styled(HeartOutlined)`
+  ${tw`text-purple-500 mr-4 mb-1 text-3xl`}
+`;
+export const StyledCommentOutlined = styled(CommentOutlined)`
+  ${tw`text-purple-500 text-3xl`}
+`;
