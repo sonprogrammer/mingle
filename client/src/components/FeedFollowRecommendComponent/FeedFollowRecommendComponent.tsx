8d41547040727ae25161acce84703f9a71de991c
Namhyun <jenple9709@gmail.com>
2023-11-16 01:15:14 +0900

    refactoring: Change feed-follow-recommend-type-name

--- client/src/components/FeedFollowRecommendComponent/FeedFollowRecommendComponent.tsx
+++ client/src/components/FeedFollowRecommendComponent/FeedFollowRecommendComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FeedFollowRecommendProps } from "../../types/FeedFollowRecommendComponent";
+import { FeedFollowRecommendProps } from "../../types/FeedFollowRecommendProps";
 import {
   FeedFollowRecommendContainer,
   ProfileImage,
