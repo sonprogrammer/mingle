69e82ed0bcdb4ddd98c1778af6c96a122a0446dd
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-16 11:59:41 +0900

    feature:JwtToken include ObjId

--- server/utils/passport/strategies/jwt-user.js
+++ server/utils/passport/strategies/jwt-user.js
@@ -9,12 +9,16 @@ opts.secretOrKey = process.env.SHA_KEY; // 사용할 시크릿 키
 
 /**
  * JWT 전략 생성
+ * userEmail: 사용자 이메일(id처럼 치부)
+ * userId: 사용자 아이디(objectId)
  */
 const jwt = new JwtStrategy(opts, async (jwt_payload, done) => {
 	try {
 		// 여기서는 토큰에서 사용자 정보 추출
 		const user = {
-			userEmail: jwt_payload.id, // 사용자 이메일 (토큰에 따라 다를 수 있음)
+			userEmail: jwt_payload.id,// 사용자 이메일 (토큰에 따라 다를 수 있음)
+			userId: jwt_payload.userId, // 사용자 아이디
+			//userId : objectId 입니다.
 		};
 
 		if (user) {
