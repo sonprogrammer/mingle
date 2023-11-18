bf3b5f38c63de5c808c7c5bca4fdb64d82102570
youngjin son <ods04138@gmail.com>
2023-11-18 13:51:30 +0900

    Refactor-Code-cleanup

--- client/src/components/PlaylistDescriptionComponent/styles.ts
+++ client/src/components/PlaylistDescriptionComponent/styles.ts
@@ -1,24 +1,22 @@
-import tw, {styled} from "twin.macro";
+import tw, { styled } from 'twin.macro'
 
-export const PlayDescriptBox = styled.div`
-    ${tw`
+export const StyledDescriptBox = styled.div`
+  ${tw`
         pt-24
         ml-10
         pb-16
 
     `}
 `
-export const PlaylistTitle = styled.div`
-    ${tw`
+export const StyledTitle = styled.div`
+  ${tw`
         text-4xl
         font-bold
     `}
 `
-export const PlaylistDescript = styled.div`
-    ${tw`
+export const StyledDescript = styled.div`
+  ${tw`
         text-2xl
         mt-8
     `}
 `
-
-
