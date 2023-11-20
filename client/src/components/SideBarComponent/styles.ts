61dd8a16cd124015224b8aa9cc9cd296399142ea
youngjin son <ods04138@gmail.com>
2023-11-20 13:59:51 +0900

    design sidebar ui

--- client/src/components/SideBarComponent/styles.ts
+++ client/src/components/SideBarComponent/styles.ts
@@ -1,111 +1,112 @@
+import tw, { styled } from "twin.macro";
 
-import tw,{ styled} from 'twin.macro';
-
-
-
-export const Nav = styled.nav`
+export const StyledNav = styled.nav`
   ${tw`
     fixed
     left-0
-      top-0
+    top-0
     bg-[#404040]
     box-border
     text-white
-    // top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#404040] text-white p-4 flex flex-col
     h-full
     border-r-indigo-500
+    
   `}
-  width: 16rem;
-`
 
-export const Logo = styled.div`
+`;
+
+export const StyledLogo = styled.div`
   ${tw`
-    text-4xl font-semibold
+    text-4xl
+    font-semibold
   `}
-`
+`;
 
-export const DivideLine = styled.div`
+export const StyledDivideLine = styled.div`
   ${tw`
     flex items-center justify-center h-[201px] border-b
   `}
-`
+`;
 
-export const ButtonsContainer = styled.div`
+export const StyledButtonsContainer = styled.div`
   ${tw`
     flex 
     flex-col 
     mt-4
     cursor-pointer
-    hover:border-slate-400 
-    justify-between
-  `}
-`
 
-export const ButtonWrapper = styled.div`
+  `}
+`;
+
+export const StyledButtonWrapper = styled.div`
+  ${tw`
+    // flex
+    // items-center space-x-3
+  `}
+  // width: 16rem;
+`;
+
+export const StyledButton = styled.div`
   ${tw`
     flex
-    items-center space-x-3
-  `}
-  width: 16rem;
-`
-
-export const Button = styled.div`
-  ${tw`
-    flex items-center space-x-3 p-2 
+     space-x-3
+      p-2
     hover:bg-zinc-900 
     hover:border-slate-400 
+    transition duration-300
+    ease-in-out
+    transform hover:scale-105
     pl-4
     py-4
+    relative
   `}
   width: 16rem;
-`
 
+  img{
+    width: 20px;
+  }
+  &:hover {
+    .additional-buttons {
+      opacity: 1; 
+      visibility: visible;
+      transform: translateY(0);
+    }
+  }
+`;
 
-export const DropdownButton = styled.div`
+export const StyledDropdownButton = styled.div`
   ${tw`
-    absolute
-    left-56
     flex
     cursor-pointer 
-    ml-4
-    w-5
   `}
-`
+`;
 
-export const Dropeddown = styled.div`
+export const StyledDropeddown = styled.div`
   ${tw`
-    bg-gray-400 absolute top-28 left-60 flex flex-col items-start rounded-lg p-2 w-32 text-center
-    ml-auto
+   flex-col rounded-lg w-full
+   bg-zinc-600
   `}
-`
+  transition: transform 0.3s ease-in-out;
+  
+`;
 
-
-
-export const DropeddownContents = styled.div`
+export const StyledDropeddownContents = styled.div`
   ${tw`
-    flex w-full hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent
+    flex 
+    p-2
+    space-x-3
+    pl-6
+    hover:bg-zinc-900 
+    hover:border-slate-400  
+    transition duration-300
+    ease-in-out
+    transform hover:scale-105
+    cursor-pointer rounded-r-lg border-l-transparent
+  
+
   `}
-`
-
-
-export const DropdownItem = styled.div`
-  ${tw`
-    p-2 cursor-pointer hover:bg-gray-200
-  `}
-`
-
-
-export const DropdownMenu = styled.div`
-  absolute 
-  flex
-  flex-col
-  top-20
-  right-20
-
-`
-
-export const DropdownList = styled.ul`
-  flex
-  flex-col 
-  gap-4
-`
+  width: 16rem;
+  span{
+    
+  }
+`;
