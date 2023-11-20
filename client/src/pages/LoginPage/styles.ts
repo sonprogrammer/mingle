ed81cfecca1580d7337e31e5c4a57d4d3f99dd86
BoyoungYun <index_1234@naver.com>
2023-11-20 14:41:28 +0900

    feat: Apply code review(boyoung) and set prettier

--- client/src/pages/LoginPage/styles.ts
+++ client/src/pages/LoginPage/styles.ts
@@ -11,7 +11,10 @@ export const StyledHelpWrapper = styled.div`
     ${tw`flex justify-between`}
 `;
 export const StyledLabel = styled.label`
-    ${tw`relative inline-flex items-center cursor-pointer`}
+    ${tw`relative inline-flex items-center cursor-pointer gap-[8px]`}
+    .span {
+        ${tw`ms-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
+    }
 `;
 export const StyledTextWrapper = styled.div<StyledTextWrapperProps>`
     ${tw`flex gap-[10px] items-center`}
@@ -26,4 +29,10 @@ export const Divider = styled.div`
 export const StyledTextButton = styled.div`
     ${tw`font-bold cursor-pointer`}
     color: #9b59b6;
+`;
+export const StyledCheckbox = styled.input`
+    ${tw`sr-only`}
+`; 
+export const StyledCheckboxChildren = styled.div`
+    ${tw`w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600`}
 `;
