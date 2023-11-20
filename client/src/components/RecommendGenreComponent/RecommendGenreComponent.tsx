f8182cb6eece7ab568c8810c57a72d1f9c397f5b
Namhyun <jenple9709@gmail.com>
2023-11-20 18:53:22 +0900

    design: Add music genre recommendation modal to registration page and Change button color

--- client/src/components/RecommendGenreComponent/RecommendGenreComponent.tsx
+++ client/src/components/RecommendGenreComponent/RecommendGenreComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   ContainerStyle,
   GenreButtonStyle,
@@ -7,7 +7,7 @@ import {
   GridStyle,
   ButtonStyle,
   ButtonContainerStyle,
-} from "./styles";
+} from './styles';
 
 interface GenreModalProps {
   isOpen: boolean;
@@ -15,11 +15,11 @@ interface GenreModalProps {
 }
 
 const genres = [
-  { id: 1, name: "발라드", image: "../../../public/img/Ballad.png" },
-  { id: 2, name: "록", image: "../../../public/img/Rock.png" },
-  { id: 3, name: "댄스", image: "../../../public/img/Dance.png" },
-  { id: 4, name: "클래식", image: "../../../public/img/Classic.png" },
-  { id: 5, name: "힙합", image: "../../../public/img/Hip-Hop.png" },
+  { id: 1, name: '발라드', image: '../../../public/img/Ballad.png' },
+  { id: 2, name: '록', image: '../../../public/img/Rock.png' },
+  { id: 3, name: '댄스', image: '../../../public/img/Dance.png' },
+  { id: 4, name: '클래식', image: '../../../public/img/Classic.png' },
+  { id: 5, name: '힙합', image: '../../../public/img/Hip-Hop.png' },
 ];
 
 export default function RecommendGenreComponent({
@@ -50,7 +50,7 @@ export default function RecommendGenreComponent({
               key={genre.id}
               onClick={() => handleGenreClick(genre.name)}
               className={
-                selectedGenres.includes(genre.name) ? "bg-blue-200" : ""
+                selectedGenres.includes(genre.name) ? 'bg-purple-200' : ''
               }
             >
               <img src={genre.image} alt={genre.name} />
--- client/src/components/RecommendGenreComponent/styles.ts
+++ client/src/components/RecommendGenreComponent/styles.ts
@@ -1,11 +1,11 @@
-import tw, { styled } from "twin.macro";
+import tw, { styled } from 'twin.macro';
 
 export const ModalStyle = styled.div`
   ${tw`fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full`}
 `;
 
 export const ContainerStyle = styled.div`
-  ${tw`relative top-20 mx-auto p-5 border w-11/12 md:w-1/3 shadow-lg rounded-md bg-white`}
+  ${tw`relative top-20 mx-auto p-5 border w-11/12 shadow-lg rounded-md bg-white`}
 `;
 
 export const GenreButtonStyle = styled.button`
@@ -21,7 +21,11 @@ export const GridStyle = styled.div`
 `;
 
 export const ButtonStyle = styled.button`
-  ${tw`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700`}
+  ${tw`px-4 py-2 text-white rounded`}
+  background-color: #9b59b6;
+  &:hover {
+    opacity: 90%;
+  }
 `;
 export const ButtonContainerStyle = styled.div`
   ${tw`flex justify-end mt-4`}
