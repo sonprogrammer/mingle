a3bda084d4f02d34a8d3e768b24b6ecd5cf49a5e
youngjin son <ods04138@gmail.com>
2023-11-21 18:21:57 +0900

    Design: Recommend playlistpage

--- client/src/pages/RecommendPlaylistPage/RecommendPlaylistPage.tsx
+++ client/src/pages/RecommendPlaylistPage/RecommendPlaylistPage.tsx
@@ -25,13 +25,35 @@ const playlistInfo = [
     likes: 777,
   },
 ];
+const GenreplaylistInfo = [
+  {
+    albumCover: '/img/AlbumSample.jpg',
+    title: '[Playlist] 쌀쌀한 늦가을에 듣기 좋은 팝송 플레이리스트',
+    likes: 222,
+  },
+  {
+    albumCover: '/img/AlbumSample.jpg',
+    title: '[Playlist] 쌀쌀한 늦가을에 듣기 좋은 팝송 플레이리스트',
+    likes: 444,
+  },
+  {
+    albumCover: '/img/AlbumSample.jpg',
+    title: '[Playlist] 쌀쌀한 늦가을에 듣기 좋은 팝송 플레이리스트',
+    likes: 666,
+  },
+  {
+    albumCover: '/img/AlbumSample.jpg',
+    title: '[Playlist] 쌀쌀한 늦가을에 듣기 좋은 팝송 플레이리스트',
+    likes: 888,
+  },
+];
 
 
 export default function FeedPage() {
   return (
     <>
       <WeatherPlaylistRecommendationComponent weather={"맑은날"} playlists={playlistInfo}/>
-      <GenrePlaylistRecommendationComponent Genre={"댄스"} playlists={playlistInfo}/>
+      <GenrePlaylistRecommendationComponent Genre={"댄스"} playlists={GenreplaylistInfo}/>
       </>
   );
 }
