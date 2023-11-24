613bdbd4cef4347e21b614627f05787052c88faa
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-11-24 14:57:56 +0900

    playList에 복수의 곡 넣기 기능 특정 플레이리스트를 get할때 음원에 대한 정보도 넘겨주기

--- server/services/playList/playAddSong.js
+++ server/services/playList/playAddSong.js
@@ -6,28 +6,35 @@ const createError = require("http-errors");
  * @param {string} songId - 추가할 음악의 ObjectId
  * @returns {Array} - [성공 여부(boolean), 메시지 객체(object)]
  */
-async function playListAddSong(playListid, songId, userId) {
-  try {
-    const playlist = await playListSchema.findById(playListid);
-    if (userId !== playlist.playListOwner.toString()) {
-      throw createError(403, "자신의 플레이리스트가 아닙니다.");
-    }
-    if (!playlist) {
-      throw createError(404, "플레이리스트를 찾을 수 없습니다.");
-    }
-    if (playlist.playListSongs.includes(songId)) {
-      throw createError(409, "이미 추가된 곡입니다.");
-    }
-    playlist.playListSongs.push(songId);
-    await playlist.save();
+async function playListAddSong(playListid, songIds, userId) {
+	try {
+		if(!songIds){
+			throw createError(400, "추가할 곡이 없습니다.");
+		}
+		const playlist = await playListSchema.findById(playListid);
+		if (userId !== playlist.playListOwner.toString()) {
+			throw createError(403, "자신의 플레이리스트가 아닙니다.");
+		}
+		if (!playlist) {
+			throw createError(404, "플레이리스트를 찾을 수 없습니다.");
+		}
+		const alreadyAddedSongs = playlist.playListSongs.filter((song) =>
+			songIds.includes(song)
+		);
+		if (alreadyAddedSongs.length > 0) {
+			throw createError(409, "이미 추가된 곡들이 포함되어 있습니다.");
+		}
+
+		playlist.playListSongs = [...playlist.playListSongs, ...songIds];
+		await playlist.save();
     return {
-      message: "플레이리스트에 곡 추가가 되었습니다",
+      message: "플레이리스트에 곡들이 추가되었습니다",
       playlistId: playlist._id.toString(),
+      playListSongs : playlist.playListSongs
     };
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+	} catch (error) {
+		throw error;
+	}
 }
 
 module.exports = { playListAddSong };
