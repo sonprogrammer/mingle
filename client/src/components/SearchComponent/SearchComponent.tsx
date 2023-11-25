d5b099cc2fd6d9ea7fb58fca7b6431fa58407c31
BoyoungYun <index_1234@naver.com>
2023-11-25 18:47:17 +0900

    feat: Implement song search get api

--- client/src/components/SearchComponent/SearchComponent.tsx
+++ client/src/components/SearchComponent/SearchComponent.tsx
@@ -4,31 +4,42 @@ import {
   StyledSearchButton,
   StyledSelectWrapper,
   StyledSelect,
-} from "./styles";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from './styles';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SearchComponent() {
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value;
-    console.log(`selected ${value}`);
+  const [keyword, setKeyword] = useState('');
+  const [type, setType] = useState('song-name');
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/search', { state: { type: type, keyword: keyword } });
   };
-
+  console.log(keyword, type);
   return (
     <div>
       <StyledSearchWrapper>
         <StyledSelectWrapper>
-          <StyledSelect defaultValue="song" onChange={handleChange}>
-            <option value="song">노래</option>
-            <option value="artist">가수</option>
-            <option value="album">앨범</option>
+          <StyledSelect
+            defaultValue="song"
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value="song-name">노래</option>
+            <option value="artist-name">가수</option>
+            <option value="playlist-name">플리</option>
           </StyledSelect>
         </StyledSelectWrapper>
         <StyledSearchInput
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') handleNavigate();
+          }}
+          onChange={(e) => setKeyword(e.target.value)}
           placeholder="원하는 검색어를 입력해주세요."
           type="text"
         />
-        <StyledSearchButton>
+        <StyledSearchButton onClick={handleNavigate}>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </StyledSearchButton>
       </StyledSearchWrapper>
