cef9701356c43a1e4c549fe34bf40d57b73352d7
youngjin son <ods04138@gmail.com>
2023-11-26 16:11:02 +0900

    Feat: edit user description

--- client/src/hooks/usePutUserDescription.ts
+++ client/src/hooks/usePutUserDescription.ts
@@ -3,24 +3,26 @@ import { useMutation } from 'react-query';
 import { UserInfo } from '../types';
 import { useAxios } from '../utils';
 
+const putUserDescription = async (
+  axiosInstance: AxiosInstance,
+  updatedInfo: Partial<UserInfo>,
+): Promise<UserInfo> => {
+  const response = await axiosInstance.put(
+    'api/account/description',
+    updatedInfo,
+  );
+  return response.data;
+};
 
-
-
-const putUserDescription = async (axiosInstance: AxiosInstance, updatedInfo:Partial<UserInfo>): Promise<UserInfo> =>{
-        const response = await axiosInstance.put('api/account/description', updatedInfo)
-        return response.data;
-    
+export function usePutUserDescription() {
+  const axiosInstance = useAxios();
+  return useMutation(
+    (updatedInfo: Partial<UserInfo>) =>
+      putUserDescription(axiosInstance, updatedInfo),
+    {
+      onSuccess: (data) => {
+        console.log('User description updated : ', data);
+      },
+    },
+  );
 }
-
-export function usePutUserDescription(){
-    const axiosInstance = useAxios();
-    return useMutation(
-        (updatedInfo: Partial<UserInfo>) => putUserDescription(axiosInstance, updatedInfo),
-        {
-            onSuccess: (data) =>{
-                console.log('User description updated : ', data)
-            }
-        }
-    )
-}
-
