27654ef5cd97f0166ce17d1adeb78b1709596063
Namhyun <jenple9709@gmail.com>
2023-11-27 11:45:39 +0900

    fix: Hooks code

--- client/src/hooks/useGetSongslike.ts
+++ client/src/hooks/useGetSongslike.ts
@@ -1,83 +1,14 @@
-import { getCookieToken } from '../utils/cookie';
+import { AxiosResponse, AxiosInstance } from 'axios';
 import { useQuery } from 'react-query';
-import axios, { AxiosError } from 'axios';
-
-async function refreshToken() {
-  const refreshToken = getCookieToken();
-
-  const tokenResponse = await fetch(
-    'http://localhost:3000/api/account/refresh',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ refreshToken }),
-    },
-  );
-
-  if (!tokenResponse.ok) {
-    throw new Error('Error refreshing access token');
-  }
-
-  return await tokenResponse.json();
-}
-
-function isTokenExpired() {
-  const expiryTime = getTokenExpiry();
-  return !expiryTime || expiryTime <= Date.now();
-}
-//토큰 만료시간을 추출해 내는 함수임
-function getTokenExpiry() {
-  const token = getCookieToken();
-
-  if (!token) {
-    return null;
-  }
-
-  try {
-    const payload = token.split('.')[1]; // JWT의 payload를 추출하고
-    const decoded = JSON.parse(atob(payload)); // Base64 디코딩을 수행한다음
-    return decoded.exp ? decoded.exp * 1000 : null; // exp 값이 있으면 밀리초로 변환하여 반환함
-  } catch (e) {
-    console.error('Error decoding token:', e);
-    return null;
-  }
-}
+import { useAxios } from '../utils';
 
 export function useGetSongslike() {
-  return useQuery(
-    'songsByLikes',
-    async () => {
-      let accessToken = getCookieToken();
+  const axios: AxiosInstance = useAxios();
 
-      if (isTokenExpired()) {
-        const tokenData = await refreshToken();
-        accessToken = tokenData.accessToken;
-      }
-
-      const response = await axios.get(
-        'http://localhost:3000/api/songs?orderby=top',
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        },
-      );
-
-      return response.data;
-    },
-    {
-      onSuccess: (data) => {
-        console.log('Data fetched successfully:', data);
-      },
-      onError: (error: unknown) => {
-        if (error instanceof AxiosError) {
-          console.error(`Error fetching songs: ${error.response?.statusText}`);
-        } else {
-          console.error('An unknown error occurred');
-        }
-      },
-    },
+  return useQuery('userUploadedSongs', () =>
+    axios.get(`/api/songs?orderby=top`).then((res: AxiosResponse) => {
+      console.log(res);
+      return res.data;
+    }),
   );
 }
--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,36 +1,46 @@
-import Axios, { AxiosHeaders } from "axios";
-import { usePostRefreshToken } from "../hooks/usePostRefreshToken";
-import { getCookieToken } from "./cookie";
-import { useRecoilValue } from "recoil";
-import { loginState } from "./state";
+import Axios, { AxiosHeaders } from 'axios';
+import { getCookieToken } from './cookie';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { loginState } from './state';
+import axios from 'axios';
 
 export const useAxios = () => {
-    const today = new Date(Date.now());
-    const { accessToken, accessExpiredDate } = useRecoilValue(loginState);
-    const refreshToken = getCookieToken();
-    const { mutate } = usePostRefreshToken(refreshToken);
+  const today = new Date(Date.now());
+  const { accessToken, accessExpiredDate } = useRecoilValue(loginState);
+  const diff = today.getTime() - new Date(accessExpiredDate).getTime();
+  const refreshToken = getCookieToken();
+  const axiosInstance = Axios.create({
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const setLogin = useSetRecoilState(loginState);
+  axiosInstance.interceptors.request.use(async (config) => {
+    if ((accessToken === '' && refreshToken) || diff >= 0) {
+      const response = await axios.post('/api/account/refresh', {
+        refreshToken: refreshToken,
+      });
+      setLogin({
+        isLogin: true,
+        accessToken: response.data.accessToken,
+        accessExpiredDate: response.data.accessExpiredDate,
+      });
+      (config.headers as AxiosHeaders).set(
+        'Authorization',
+        `Bearer ${response.data.accessToken}`,
+      );
+    } // 토큰이 유실되었을 때 또는 access Token이 만료 되었을 때 재발급 로직(refresh Token은 존재함)
 
-    const axios = Axios.create({
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
-        },
-    });
-    axios.interceptors.request.use((config) => {
-        if(!accessToken && refreshToken || new Date(accessExpiredDate) <= today)
-        {
-            mutate();
-        } // 토큰이 유실되었을 때 또는 access Token이 만료 되었을 때 재발급 로직(refresh Token은 존재함)
-        (config.headers as AxiosHeaders).set('Authorization', `Bearer ${accessToken}`);
-        return config;
-    });
-    axios.interceptors.response.use(
-        (response) => {
-            return response.data;
-        },
-        (error) => {
-            return Promise.reject(error);
-        }
-    );
-    return axios;
-}
+    return config;
+  });
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response.data;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+  return axiosInstance;
+};
