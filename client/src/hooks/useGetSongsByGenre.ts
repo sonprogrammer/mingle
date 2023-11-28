d23d543a3e2d83d7abbf2a7b04fb1ab0f8b62bd3
BoyoungYun <index_1234@naver.com>
2023-11-28 15:29:05 +0900

    fix: Fix response data error

--- client/src/hooks/useGetSongsByGenre.ts
+++ client/src/hooks/useGetSongsByGenre.ts
@@ -4,10 +4,10 @@ import { Songs } from '../types';
 import { useAxios } from '../utils';
 
 const getSongsByGenre = async (axiosInstance: AxiosInstance, genre: string | undefined, pageNum: number): Promise<Songs> => {
-	const response: Songs | PromiseLike<Songs> = await axiosInstance.get(		
+	const response = await axiosInstance.get(		
         `/api/songs?category=${genre}&page=${pageNum}&pageSize=8`
 	);
-	return response;
+	return response.data;
 };
 export function useGetSongsByGenre(genre: string | undefined, pageNum: number) {
     const axiosInstance = useAxios();
